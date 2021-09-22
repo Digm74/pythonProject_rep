be832f7bdded8ce3c026555292359c3737c9acf3
Digm74 <i.devochko@gmail.com>
2021-09-22 22:14:53 +0300

    add less7 while

--- less7.py
+++ less7.py
@@ -13,3 +13,16 @@ print(a)
 while 3 in a:
     a.remove(3)
     print(a)
+
+s = 'qwerty134678QWE,.JK:'
+while len(s)>0:
+    bukva = s[0]
+    if bukva > 'a' and bukva < 'z':
+        print(bukva, 'is small')
+    elif bukva > 'A' and bukva < 'Z':
+        print(bukva, 'is big')
+    elif bukva > '0' and bukva < '9':
+        print(bukva, 'is digit')
+    else:
+        print(bukva, 'is simbol')
+    s = s[1:]
